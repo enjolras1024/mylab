e156283ae5716bf5496562e7bfcb0fafbd998c32
enjolras1024 <enjolras1024@163.com>
2016-04-01 18:43:52 +0800

    add expt 4

--- dist/expt2.js
+++ dist/expt2.js
@@ -5442,14 +5442,14 @@ ENJ.Lab = (function() {
 //##############################################################################
 // src/exit.js
 //##############################################################################
-var stats = new Stats();
-
-stats.setMode(0); // 0: fps, 1: ms
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-
-document.body.appendChild(stats.domElement);
+//var stats = new Stats();
+//
+//stats.setMode(0); // 0: fps, 1: ms
+//stats.domElement.style.position = 'absolute';
+//stats.domElement.style.left = '0px';
+//stats.domElement.style.top = '0px';
+//
+//document.body.appendChild(stats.domElement);
 
 
 var lab = new ENJ.Lab('stage');
@@ -5475,7 +5475,7 @@ function update(event) {
     lab.update(event);
   }
 
-  stats.update();
+  //stats.update();
   //requestAnimationFrame(update);
 }
 //update();
